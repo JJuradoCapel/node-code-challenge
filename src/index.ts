3d5bf46ee695b2dba103dd3607f5ee1dc1533a68
Juan Antonio Jurado <juanan@strivelabs.io>
2021-07-26 18:09:20 +0200

    Fix exception with file that are only numbers

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { parseFile, writeResult } from './utils/files';
 // Remove the exec path and the script location and parse the arguments
 const args = parseArgs(process.argv.slice(2));
 (async () => {
-  const [map, robots, actions] = await parseFile(args._[0]);
+  const [map, robots, actions] = await parseFile(String(args._[0]));
   robots.forEach((robot, index) => map.simulate(robot, actions[index]));
-  await writeResult(robots, args._[1]);
+  await writeResult(robots, String(args._[1]));
 })();
